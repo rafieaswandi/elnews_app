d5102488ae2aa93543fd0b339f488102984e037c
rafieaswandi <muhammadrafieaswandi@gmail.com>
2026-04-13 08:05:26 +0700

    l

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,7 +13,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Newen'),
+        title: const Text('ElNews'),
         centerTitle: true,
         bottom: TabBar(
           controller: homeController.tabBarController,
